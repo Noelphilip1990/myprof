0c12de7f9509bec7b89f20eee8228b5c73fda477
Adam Philip <noyo1000gt@gmail.com>
2025-01-16 09:21:47 +0530

    add package netlify

--- src/app/app.config.server.ts
+++ src/app/app.config.server.ts
@@ -1,6 +1,7 @@
 import { mergeApplicationConfig, ApplicationConfig } from '@angular/core';
 import { provideServerRendering } from '@angular/platform-server';
 import { appConfig } from './app.config';
+import { render } from '@netlify/angular-runtime/common-engine';
 
 const serverConfig: ApplicationConfig = {
   providers: [
